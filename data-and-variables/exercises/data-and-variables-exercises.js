56558852121a841aea2a6531d73d9a492338b0c2
colleenrust <colleenrust524@gmail.com>
2024-04-23 15:43:43 -0500

    calculated distance to mars and the moon in days. hooray!

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,11 +1,32 @@
 // Declare and assign the variables below
+let spaceShuttle = 'Determination';
+let shuttleSpeedMph = 17500;
+let distanceToMarsKm = 225000000;
+let distancetoMoonKm = 384400;
+const milesPerKm = 0.621;
+
+
 
 // Use console.log to print the 'typeof' each variable. Print one item per line.
+// console.log(typeof (spaceShuttle));
+// console.log(typeof (shuttleSpeedMph));
+// console.log(typeof (distanceToMarsKm));
+// console.log(typeof (distancetoMoonKm));
+// console.log(typeof (milesPerKM));
+
 
 // Calculate a space mission below
+let milesToMars = distanceToMarsKm * milesPerKm;
+let hoursToMars = milesToMars / shuttleSpeedMph;
+let daysToMars = hoursToMars / 24;
+
 
 // Print the results of the space mission calculations below
+console.log(spaceShuttle + " will take " + daysToMars + " days to reach Mars");
 
 // Calculate a trip to the moon below
-
+let milesToMoon = distancetoMoonKm * milesPerKm;
+let hoursToMoon = milesToMoon / shuttleSpeedMph;
+let daysToMoon = hoursToMoon / 24;
 // Print the results of the trip to the moon below
+console.log(spaceShuttle + " will take " + daysToMoon + " days to reach the Moon");
